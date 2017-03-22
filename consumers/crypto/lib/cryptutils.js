6bad32b3636261a9a0229c5a47ea66ff4a573f0f
Kaan <Kaan_K193@hotmail.com>
2017-03-23 02:38:13 +0300

    Remove unnecessary crypto methods

--- consumers/crypto/lib/cryptutils.js
+++ consumers/crypto/lib/cryptutils.js
@@ -1,29 +1,12 @@
 const crypto = require('crypto');
-const hash = crypto.createHash('sha256');
 
-
-class CryptUtils {
-
+module.exports = {
   /**
-   * Creates hash of given password with random 16 byte salt
-   * @return {String}
-   */
-  static async createHash(password) => {
-    try {
-      let salt = await this.generateSalt();
-      let passwordDigest = await this.generateHash(password, salt);
-      return passwordDigest.toString('hex');
-    } catch(err) {
-      throw new Error(err);
-    }
-  }
-
-  /**
-   * Creates a SHA-256 digest of given password and salt
+   * Creates a PBKDF2 + SHA-256 digest of given password and salt
    * @param {String} password
-   * @return {String}
+   * @return Promise
    */
-  static async generateHash(password, salt) => {
+  generateHash: (password, salt) => {
     return new Promise((resolve, reject) => {
       crypto.pbkdf2(password, salt, 24000, 256, 'sha256', (err, key) => {
         if (err) {
@@ -33,13 +16,13 @@ class CryptUtils {
         resolve(key.toString('hex'));
       });
     });
-  }
+  },
 
   /**
    * Generates random 32 byte. See crypto documentation for byte length.
    * @return {String} HexPassword
    */
-  static async generateSalt() => {
+  generateSalt: async () => {
     try {
       let buf = crypto.randomBytes(32);
       return buf.toString('hex');
@@ -47,14 +30,4 @@ class CryptUtils {
       throw new Error(err);
     }
   }
-
-  /**
-   * Compares given hash and the password with the salt combined.
-   * @return {Boolean}
-   */
-  static async compareHash(hash, password, salt) => {
-    let generatedHash = await this.generateHash(password, salt);
-    return hash === generatedHash;
-  }
-
-}
+};
