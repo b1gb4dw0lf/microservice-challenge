3608ccfcc63f947f90e9371d0986ea333d74fcca
Kaan <Kaan_K193@hotmail.com>
2017-03-23 23:48:12 +0300

    Add basic user api

--- producer/routers/user.js
+++ producer/routers/user.js
@@ -4,10 +4,10 @@ const microserviceKit = require('../lib/microservice-kit');
 const router = new Router();
 
 
-router.get('/:id', async (ctx, next) => {
+router.get('/', async (ctx, next) => {
   try {
     let userQueue = microserviceKit.amqpKit.getQueue('user');
-    let user = await userQueue.sendEvent('get', {id: this.params.id});
+    let user = await userQueue.sendEvent('get', {email: ctx.state.user.email});
     ctx.body = user;
   } catch (err) {
     console.log(err);
@@ -22,7 +22,11 @@ router.post('/', async (ctx, next) => {
     ctx.status = 200;
   } catch (err) {
     console.log(err);
-    ctx.status = 500;
+    if (err.code = 11000) {
+      ctx.status = 404;
+    } else {
+      ctx.status = 500;      
+    }
   }
 });
 
