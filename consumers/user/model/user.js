b228a5dadb6c68e23bb0bf45bbacb25cbee88009
Kaan <Kaan_K193@hotmail.com>
2017-03-23 11:53:08 +0300

    Add user schema

--- consumers/user/model/user.js
+++ consumers/user/model/user.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+let User = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true},
+  badges: [{
+    _id: false,
+    badge: {type: Schema.ObjectId, ref: 'Badge'}
+  }],
+  password: {type: String, required: true},
+  salt: {type: String},
+  createdAt: {type: Date, default: Date.now}
+});
+
+
+module.exports = mongoose.model('User', User);
