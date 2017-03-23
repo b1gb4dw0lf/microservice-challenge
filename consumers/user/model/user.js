5c13753bc0e99eb4d05938c88809ad9999feb1f5
Kaan <Kaan_K193@hotmail.com>
2017-03-23 11:53:33 +0300

    Hash passwords before saving

--- consumers/user/model/user.js
+++ consumers/user/model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const microserviceKit = require('microservice-kit');
 
 
 let User = new Schema({
@@ -13,6 +14,19 @@ let User = new Schema({
   password: {type: String, required: true},
   salt: {type: String},
   createdAt: {type: Date, default: Date.now}
+});
+
+
+User.pre('save', async (next) => {
+  try {
+    let cryptoQueue = microserviceKit.amqpKit.getQueue('crypto');
+    let cryptoResponse = await cryptoQueue.sendEvent('createPassword', {password: this.password});
+    this.password = cryptoResponse.password;
+    this.salt = cryptoResponse.salt;
+    next();
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 
