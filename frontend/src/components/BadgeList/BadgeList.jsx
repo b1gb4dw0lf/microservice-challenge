7314b9528250588e9c71cc9dd2c6b11c9bca81bf
Kaan <Kaan_K193@hotmail.com>
2017-03-30 02:45:23 +0300

    Implement actual badge list functionality

--- frontend/src/components/BadgeList/BadgeList.jsx
+++ frontend/src/components/BadgeList/BadgeList.jsx
@@ -1,19 +1,73 @@
 import React from 'react';
+import axios from 'axios';
 import Badge from '../Badge/Badge.jsx';
 
 import './BadgeList.less';
 
 class BadgeList extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      badges: [],
+      userBadges: []
+    }
+  }
+
+  componentWillMount() {
+    let badgeRequest = axios.get('/api/badge');
+    let userRequest = axios.get('/api/user');
+
+    axios.all([badgeRequest, userRequest])
+      .then(axios.spread((badgeRes, userRes) => {
+        this.setState({
+          badges: badgeRes.data,
+          userBadges: userRes.data.badges
+        });
+      }))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <div className="badge-list-container">
+    let counter = 0;
+    let badgeList = [];
+
+    if (this.state.badges) {
+      if (this.state.userBadges) {
+        this.state.userBadges.forEach((item) => {
+          badgeList.push(<Badge key={counter} name={item.name} url="../../assests/crown.png"/>)
+          counter++;
+        });
+      }
+
+      this.state.badges.forEach((item) => {
+        if (this.state.userBadges) {
+          let isExist = false;
+          this.state.userBadges.forEach((innerItem) => {
+            if (item.name == innerItem.name)
+              isExist = true;
+          });
+
+          if (!isExist)
+            badgeList.push(<Badge key={counter} name={item.name} url="../../assests/lock-badge.png"/>)
+
+          counter++;
+        }
+      });
+    } else {
+      return (<div className="badge-list-container">
+        <h3>No badges available.</h3>
+      </div>)
+    }
+
+    return (<div className="badge-list-container">
       <h2>Badge List</h2>
       <div className="badge-list">
-        <Badge url="../../assests/lock-badge.png"/>
-        <Badge url="../../assests/lock-badge.png"/>
-        <Badge url="../../assests/lock-badge.png"/>
-        <Badge url="../../assests/lock-badge.png"/>
+        {badgeList}
       </div>
-    </div>
+    </div>)
   }
 }
 
